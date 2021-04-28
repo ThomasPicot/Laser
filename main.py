6e62407e5ba7df9dab9fd7cd2ae0f2d87f0e988e
ThomasPicot <71181911+ThomasPicot@users.noreply.github.com>
2021-04-28 14:00:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from PyQt5 import QtWidgets
-from Laser.Views.openview import OpenView
+from Views.openview import OpenView
 
 if __name__ == "__main__":
     import sys
 
     app = QtWidgets.QApplication(sys.argv)
     Form = QtWidgets.QWidget()
-    ui = OpenView()
+    ui = OpenView(Form)
     ui.setupUi(Form)
     Form.show()
     sys.exit(app.exec_())
